8396772fde9680308f6c3780da1cf1b567d71bdd
Ata ul nasar <ataulnasar@gmail.com>
2026-01-22 10:17:22 +0100

    Add Init for backend

---
+++ backend/src/main/java/com/ata/supportcopilot/SupportcopilotApplication.java
@@ -0,0 +1,13 @@
+package com.ata.supportcopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportcopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportcopilotApplication.class, args);
+	}
+
+}
